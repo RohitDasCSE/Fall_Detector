d7fa9015bdca7e4c123acdc13162bf1d90183a8d
Rohit Das <rohitdascse@gmail.com>
2025-07-27 11:52:55 +0530

    Replace deprecated I2S_COMM_FORMAT_I2S_MSB with I2S_COMM_FORMAT_STAND_I2S

--- src/main.cpp
+++ src/main.cpp
@@ -263,7 +263,7 @@ void initAudioInference() {
     .sample_rate = 16000,
     .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
     .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-    .communication_format = I2S_COMM_FORMAT_I2S_MSB,
+    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
     .dma_buf_count = 8,
     .dma_buf_len = 256,
